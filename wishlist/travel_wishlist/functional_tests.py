33e5261ce44c04134d50a1d7325075f8a92c5514
Marie8bit <yd7581ku@go.minneapolis.edu>
2017-03-27 22:21:29 -0500

    add some tests and functional tests

--- wishlist/travel_wishlist/functional_tests.py
+++ wishlist/travel_wishlist/functional_tests.py
@@ -7,12 +7,46 @@ class HomePageTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
+
+
     def tearDown(self):
         self.browser.quit()
+
+
     def test_get_home_page_list_of_places(self):
         self.browser.get(self.live_server_url)
-        assert 'Wishlist' in self.browser.title
+
         assert 'Tokyo' in self.browser.page_source
         assert 'New York' in self.browser.page_source
         assert 'San Francisco' in self.browser.page_source
+        assert 'Moab' in self.browser.page_source
+
+
+    def test_get_list_of_visited_places(self):
+        self.browser.get(self.live_server_url + '/city/1/edit')
+        assert 'Wishlist' in self.browser.header
+        assert 'Tokyo' in self.browser.page_source
+        assert 'New York' not in self.browser.page_source
+        assert 'San Francisco' not in self.browser.page_source
+        assert 'Moab' not in self.browser.page_source
+
+        self.browser.get(self.live_server_url + '/city/2/edit')
+        assert 'Wishlist' in self.browser.header
+        assert 'Tokyo' not in self.browser.page_source
+        assert 'New York' in self.browser.page_source
+        assert 'San Francisco' not in self.browser.page_source
+        assert 'Moab' not in self.browser.page_source
+
+        self.browser.get(self.live_server_url + '/city/3')
+        assert 'Wishlist' in self.browser.header
+        assert 'Tokyo' not in self.browser.page_source
+        assert 'New York' not in self.browser.page_source
+        assert 'San Francisco' in self.browser.page_source
+        assert 'Moab' not in self.browser.page_source
+
+        self.browser.get(self.live_server_url + '/city/4')
+        assert 'Wishlist' in self.browser.header
+        assert 'Tokyo' not in self.browser.page_source
+        assert 'New York' not in self.browser.page_source
+        assert 'San Francisco' not in self.browser.page_source
         assert 'Moab' in self.browser.page_source
--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -14,5 +14,70 @@ class TestWishList(TestCase):
         response = self.client.get(reverse('place_list'))
         self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
         data_rendered = list(response.context['places'])
-        data_expected = list(Place.objects.filter(visited=False))
+        data_expected = list(Place.objects.order_by('name'))
         self.assertCountEqual(data_rendered, data_expected)
+
+
+
+class TestAddNewPlace(TestCase):
+
+    def test_add_new_unvisited_place_to_wishlist(self):
+
+        response =  self.client.post(reverse('place_list'), { 'name': 'Tokyo', 'visited': False}, follow=True)
+
+        # Check correct template was used
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        # What data was used to populate the template?
+        response_places = response.context['places']
+        # Should be 1 item
+        self.assertEqual(len(response_places), 1)
+        tokyo_response = response_places[0]
+
+        # Expect this data to be in the database. Use get() to get data with
+        # the values expected. Will throw an exception if no data, or more than
+        # one row, matches. Remember throwing an exception will cause this test to fail
+        tokyo_in_database = Place.objects.get(name="Tokyo", visited=False)
+
+        # Is the data used to render the template, the same as the data in the database?
+        self.assertEqual(tokyo_response, tokyo_in_database)
+
+        # And add another place - still works?
+        response =  self.client.post(reverse('place_list'), { 'name': 'Yosemite', 'visited': False}, follow=True)
+
+        # Check correct template was used
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        # What data was used to populate the template?
+        response_places = response.context['places']
+        # Should be 2 items
+        self.assertEqual(len(response_places), 2)
+
+        # Expect this data to be in the database. Use get() to get data with
+        # the values expected. Will throw an exception if no data, or more than
+        # one row, matches. Remember throwing an exception will cause this test to fail
+        place_in_database = Place.objects.get(name="Yosemite", visited=False)
+        place_in_database = Place.objects.get(name="Tokyo", visited=False)
+
+        places_in_database = Place.objects.all()  # Get all data
+
+        # Is the data used to render the template, the same as the data in the database?
+        self.assertCountEqual(list(places_in_database), list(response_places))
+
+#add place to wishlist homepage whether it is visited or not
+    def test_add_new_visited_place_to_wishlist(self):
+
+        response =  self.client.post(reverse('place_list'), { 'name': 'Tokyo', 'visited': True }, follow=True)
+
+        # Check correct template was used
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        # What data was used to populate the template?
+        response_places = response.context['places']
+        # Should be 0 items - have not added any un-visited places
+        self.assertEqual(len(response_places), 1)
+
+        # Expect this data to be in the database. Use get() to get data with
+        # the values expected. Will throw an exception if no data, or more than
+        # one row, matches. Remember throwing an exception will cause this test to fail
+        place_in_database = Place.objects.get(name="Tokyo", visited=True)
